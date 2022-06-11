0b08b05c57612c06642422ee2929d4e2f4f14c3c
Michal Radomski <michalra@op.pl>
2022-06-11 20:25:53 +0200

    Currencies are fetched from NBP Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./App.scss";
 class App extends React.Component<{}, {}> {
+  getCurrencies = () => {
+    fetch("https://api.nbp.pl/api/exchangerates/tables/A")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log({data});
+        const currencies = data[0].rates;
+        console.log({currencies});
+      });
+  };
+
+  componentDidMount() {
+    this.getCurrencies();
+  }
+
   render() {
     return <React.Fragment>NBP API App</React.Fragment>;
   }
