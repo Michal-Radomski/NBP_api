b8f6fcb66cbe972f8ae7594f099d459c858bdc9b
Michal Radomski <michalra@op.pl>
2022-06-11 23:23:15 +0200

    Currencies list is rendered - bootstrap was added

--- src/components/CurrencyList.tsx
+++ src/components/CurrencyList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useSelector} from "react-redux";
+import Card from "react-bootstrap/Card";
 
 const CurrencyList = (): JSX.Element => {
   const {currencies} = useSelector((state: State) => state?.rates ?? undefined) as State;
@@ -7,16 +8,27 @@ const CurrencyList = (): JSX.Element => {
 
   const currenciesList =
     currencies !== undefined
-      ? currencies.map((currency: Currency) => <li key={currency.code}>{currency.code}</li>)
+      ? currencies.map((currency: Currency) => (
+          <Card
+            key={currency.code}
+            border="info"
+            style={{width: "300px", height: "120px", borderRadius: "0.25rem"}}
+            as="div"
+          >
+            <Card.Body>
+              <Card.Title>{currency.code}</Card.Title>
+              <Card.Text>
+                Name: {currency.currency}
+                <br />
+                Rate: {currency.mid}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))
       : "No Data - Loading";
   // console.log("currenciesList:", currenciesList);
 
-  return (
-    <div>
-      List of Currencies
-      <ul>{currenciesList}</ul>
-    </div>
-  );
+  return <React.Fragment>{currenciesList}</React.Fragment>;
 };
 
 export default CurrencyList;
