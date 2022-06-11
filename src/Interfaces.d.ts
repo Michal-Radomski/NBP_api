e42ebcc19d0fbee1af96c91f7311a006226f596a
Michal Radomski <michalra@op.pl>
2022-06-11 23:04:01 +0200

    Currencies list is rendered

--- src/Interfaces.d.ts
+++ src/Interfaces.d.ts
@@ -3,3 +3,9 @@
 type State = ReturnType<typeof store.getState>;
 type Dispatch = typeof store.dispatch;
 type Fetch = typeof store.fetch;
+
+interface Currency {
+  currency: string;
+  code: string;
+  mid: number;
+}
--- src/components/CurrencyList.tsx
+++ src/components/CurrencyList.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import {useSelector} from "react-redux";
 
 const CurrencyList = (): JSX.Element => {
-  const currenciesList = useSelector((state: State) => state?.rates ?? "No Data") as State;
+  const {currencies} = useSelector((state: State) => state?.rates ?? undefined) as State;
+  console.log("currenciesList.currencies:", currencies);
 
-  console.log("currenciesList:", currenciesList);
-  return <div>CurrencyList</div>;
+  const currenciesList =
+    currencies !== undefined
+      ? currencies.map((currency: Currency) => <li key={currency.code}>{currency.code}</li>)
+      : "No Data - Loading";
+  // console.log("currenciesList:", currenciesList);
+
+  return (
+    <div>
+      List of Currencies
+      <ul>{currenciesList}</ul>
+    </div>
+  );
 };
 
 export default CurrencyList;
