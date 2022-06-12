174da30316790b7fc3d0fe01588237b9596d7d69
Michal Radomski <michalra@op.pl>
2022-06-12 22:00:08 +0200

    FavouriteCurrencies finished

--- src/components/FavouriteCurrencies.tsx
+++ src/components/FavouriteCurrencies.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import styled from "styled-components";
+import {Card} from "react-bootstrap";
 
 import {H2} from "./CurrencyList";
 import ConfirmationModal from "./ConfirmationModal";
 
 const Container = styled.div`
   width: 90%;
-  height: 120px;
+  height: 192px;
   display: block;
   margin-left: auto;
   margin-right: auto;
-  background-color: whitesmoke;
+  background-color: inherit;
+`;
+
+const CurrencyList = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-items: center;
+  gap: 3px;
 `;
 
 const FavouriteCurrencies = (): JSX.Element => {
@@ -19,7 +30,38 @@ const FavouriteCurrencies = (): JSX.Element => {
 
   const favouriteCurrencies =
     currencies !== undefined ? currencies.filter((currency: Currency) => currency.isFavourite === true) : [];
-  console.log("favouriteCurrencies:", favouriteCurrencies);
+  // console.log("favouriteCurrencies:", favouriteCurrencies);
+
+  const favouriteCurrenciesList =
+    favouriteCurrencies.length >= 1 ? (
+      <CurrencyList>
+        {favouriteCurrencies.map((currency: Currency) => (
+          <Card
+            key={currency.code}
+            border="success"
+            style={{
+              width: "95px",
+              height: "62px",
+              borderRadius: "0.2rem",
+              textAlign: "center",
+            }}
+          >
+            <Card.Title style={{marginBottom: "2px", textAlign: "center"}}>{currency.code}</Card.Title>
+            <Card.Body style={{padding: "5px"}}>
+              <Card.Text>
+                Rate: <span style={{fontWeight: "bold"}}>{currency.mid.toFixed(3)}</span>{" "}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CurrencyList>
+    ) : (
+      <div style={{color: "darkmagenta", textAlign: "center", padding: "20px 0"}}>
+        <h1>List is empty...</h1>
+        <h2>Click on a currency to add it to your favorites list</h2>
+        <h2>Click on a currency again to remove it from the favorites list</h2>
+      </div>
+    );
 
   return (
     <React.Fragment>
@@ -29,7 +71,7 @@ const FavouriteCurrencies = (): JSX.Element => {
         <ConfirmationModal />
       </div>
 
-      <Container>empty</Container>
+      <Container>{favouriteCurrenciesList}</Container>
     </React.Fragment>
   );
 };
