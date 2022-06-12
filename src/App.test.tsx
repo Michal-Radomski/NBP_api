00d9af03ef330a02b7f7c207359c25dd0a915ff3
Michal Radomski <michalra@op.pl>
2022-06-13 01:36:09 +0200

    Test with React Testing Library added

---
+++ src/App.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import App from './App';
+
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
+test('renders learn react link', async () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  // screen.debug();
+
+  const linkElement1 = screen.getByText(/List is empty.../i);
+  expect(linkElement1).toBeInTheDocument();
+  const linkElement2 = screen.getByText(/List of Favourite Currencies/i);
+  expect(linkElement2).toBeInTheDocument();
+  const linkElement3 = screen.getByText(/List of Available Currencies/i);
+  expect(linkElement3).toBeInTheDocument();
+
+  const button = screen.getByRole('button', {name: 'Reset Favourites'});
+  fireEvent.click(button);
+  await screen.findByText('Yes');
+  await screen.findByText('No');
+
+  setTimeout(() => {
+    const linkElement4 = screen.getByText(/dolar amerykański/i);
+    expect(linkElement4).toBeInTheDocument();
+    const linkElement5 = screen.getByText(/euro/i);
+    expect(linkElement5).toBeInTheDocument();
+  }, 1000);
+});
